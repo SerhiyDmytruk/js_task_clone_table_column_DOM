4a4700a3ca7989612b4e14a0e7ba3b2a39b7ef39
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-10-28 16:00:30 +0200

    Remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,24 +4,18 @@
 const table = document.querySelector('table');
 
 function extendNonBodyBlock(block) {
-  const clone = block.rows[0].cells[1].cloneNode(true);
-  const targetCell = block.rows[0].cells[3];
+  Array.from(block.rows).forEach((item) => {
+    const clone = item.cells[1].cloneNode(true);
+    const targetCell = item.cells[block.rows[0].cells.length - 2];
 
-  targetCell.after(clone);
+    targetCell.after(clone);
+  });
 }
 
 const tHead = table.tHead;
 const tFoot = table.tFoot;
-
-[tHead, tFoot].forEach((block) => {
-  extendNonBodyBlock(block);
-});
-
 const tBody = table.tBodies[0];
 
-Array.from(tBody.rows).forEach((item) => {
-  const clone = item.cells[1].cloneNode(true);
-  const targetCell = item.cells[3];
-
-  targetCell.after(clone);
+[tHead, tFoot, tBody].forEach((block) => {
+  extendNonBodyBlock(block);
 });
