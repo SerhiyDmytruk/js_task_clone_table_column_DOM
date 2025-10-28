90d908756c23f6d377b0d237de28afa2a984b482
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-10-28 15:35:07 +0200

    Solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
 // write your code here
+const table = document.querySelector('table');
+
+function extendNonBodyBlock(block) {
+  const clone = block.rows[0].cells[1].cloneNode(true);
+  const targetCell = block.rows[0].cells[3];
+
+  targetCell.after(clone);
+}
+
+const tHead = table.tHead;
+const tFoot = table.tFoot;
+
+[tHead, tFoot].forEach((block) => {
+  extendNonBodyBlock(block);
+});
+
+const tBody = table.tBodies[0];
+
+Array.from(tBody.rows).forEach((item) => {
+  const clone = item.cells[1].cloneNode(true);
+  const targetCell = item.cells[3];
+
+  targetCell.after(clone);
+});
